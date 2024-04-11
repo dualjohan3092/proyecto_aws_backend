3321024d0fdacc55d6c88f89c6b1a8c62ff0b108
Johan.Bohorquez <johan.bohorquez@perficient.com>
2024-04-11 18:44:53 -0500

    Firs push

---
+++ backend_app/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.BACKEND_DOCKER_PORT;
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.send('¡Hola Mundo desde el backend!');
+});
+
+app.listen(port, () => {
+  console.log('El servidor del backend está corriendo en', process.env.BACKEND_DOCKER_PORT);
+});
