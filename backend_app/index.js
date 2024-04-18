835832aa20c6c53da3b3f44e3bfe6876b5f92b71
johanbohorquez2021 <johan.bohorquez@perficient.com>
2024-04-18 17:43:41 -0500

    elven change on the index.js logic

--- backend_app/index.js
+++ backend_app/index.js
@@ -3,6 +3,25 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.BACKEND_DOCKER_PORT || 3000;
+const backendIP = process.env.BACKEND_IP || 'localhost'; 
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.send('¡Hola Mundo desde el backend!');
+});
+
+app.listen(port, backendIP, () => {
+  console.log('El servidor del backend está corriendo en', process.env.BACKEND_IP + ':' + process.env.BACKEND_DOCKER_PORT);
+});
+
+
+/*
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.BACKEND_DOCKER_PORT || 3000;
 app.use(cors());
 
 app.get('/api', (req, res) => {
@@ -12,3 +31,4 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log('El servidor del backend está corriendo en', process.env.BACKEND_DOCKER_PORT);
 });
+*/
